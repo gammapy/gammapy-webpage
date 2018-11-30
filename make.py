89a021fddb26fec6086b32f46369f419f5d7e2a1
Jose Enrique Ruiz <jer@iaa.es>
2018-11-30 12:27:59 +0100

    Update make.py to include new datasets in json file

--- make.py
+++ make.py
@@ -12,20 +12,23 @@ import click
 
 log = logging.getLogger(__name__)
 
-GAMMAPY_EXTRA = Path(os.environ["GAMMAPY_EXTRA"]) / "datasets"
-
 
 class Dataset:
     """Dataset base class.
 
-    A dataset has a name, which is the local folder name.
-    It also has a description and list of files,
-    where each file has a given remove URL (TODO: and MD5 checksum and file size).
+    The Dataset class has a local_repo property where to scan the content
+    and a base_url to build access links for each file.
+
+    A dataset has a name as identifier.
+    It also has a description and list of files, each file has a given URL
+    and a path that tells you where the file will be placed when downloaded.
+    TODO: Add MD5 checksums and file sizes.
 
     If you want to add a dataset, make a new class and add it to the list below.
     """
 
-    _url = "https://github.com/gammapy/gammapy-extra/raw/master/datasets"
+    base_url = "https://github.com/gammapy/gammapy-extra/raw/master/datasets"
+    local_repo = Path(os.environ["GAMMAPY_EXTRA"]) / "datasets"
 
     @property
     def record(self):
@@ -37,9 +40,10 @@ class Dataset:
 
     @property
     def files(self):
-        for path in (GAMMAPY_EXTRA / self.name).glob("**/*.*"):
-            urlpath = path.as_posix().replace(GAMMAPY_EXTRA.as_posix(), "")
-            yield {"path": urlpath[1:], "url": self._url + urlpath}
+        for path in (self.local_repo / self.name).glob("**/*.*"):
+            if not path.name.startswith('.'):
+                urlpath = path.as_posix().replace(self.local_repo.as_posix(), "")
+                yield {"path": urlpath[1:], "url": self.base_url + urlpath}
 
 
 class DatasetCTA1DC(Dataset):
@@ -92,6 +96,40 @@ class DatasetEBL(Dataset):
     description = "tbd"
 
 
+class DatasetGammaCat(Dataset):
+    name = "gamma-cat"
+    description = "tbd"
+    files = [
+        {
+            "path": "gamma-cat/gammacat.fits.gz",
+            "url": "https://github.com/gammapy/gamma-cat/raw/master/output/gammacat.fits.gz"
+        }
+    ]
+
+
+class DatasetFermiLat(Dataset):
+    name = "fermi-lat-data"
+    description = "tbd"
+    files = [
+        {
+            "path": "fermi_3fhl/fermi_3fhl_events_selected.fits.gz",
+            "url": "https://github.com/gammapy/gammapy-fermi-lat-data/raw/master/3fhl/allsky/fermi_3fhl_events_selected.fits.gz"
+        },
+        {
+            "path": "fermi_3fhl/fermi_3fhl_exposure_cube_hpx.fits.gz",
+            "url": "https://github.com/gammapy/gammapy-fermi-lat-data/raw/master/3fhl/allsky/fermi_3fhl_exposure_cube_hpx.fits.gz"
+        },
+        {
+            "path": "fermi_3fhl/fermi_3fhl_psf_gc.fits.gz",
+            "url": "https://github.com/gammapy/gammapy-fermi-lat-data/raw/master/3fhl/allsky/fermi_3fhl_psf_gc.fits.gz"
+        },
+        {
+            "path": "fermi_3fhl/iso_P8R2_SOURCE_V6_v06.txt",
+            "url": "https://github.com/gammapy/gammapy-fermi-lat-data/raw/master/isodiff/iso_P8R2_SOURCE_V6_v06.txt"
+        }
+    ]
+
+
 class DatasetIndex:
     path = "download/data/gammapy-data-index.json"
     datasets = [
@@ -105,6 +143,8 @@ class DatasetIndex:
         DatasetImages,
         DatasetJointCrab,
         DatasetEBL,
+        DatasetFermiLat,
+        DatasetGammaCat,
     ]
 
     def make(self):

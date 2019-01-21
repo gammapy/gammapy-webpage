7109d4c884a81543ff0796d34e62d13589a55dfe
adonath <axel.donath@mpi-hd.mpg.de>
2019-01-21 16:15:29 +0100

    Add 3FHL GC dataset

--- make.py
+++ make.py
@@ -106,7 +106,7 @@ class DatasetJointCrab(Dataset):
     description = "tbd"
 
     base_url = "https://github.com/open-gamma-ray-astro/joint-crab/raw/master/results/spectra"
-    local_repo = Path(os.environ["JOINT_CRAB"])/'results'/'spectra'
+    local_repo = Path(os.environ["JOINT_CRAB"]) / 'results' / 'spectra'
     files = []
 
     for path in (local_repo).glob("**/*.*"):
@@ -126,7 +126,7 @@ class DatasetGammaCat(Dataset):
     local_repo = Path(os.environ["GAMMA_CAT"])
     files = []
 
-    pathlist = [str(Path('output')/'gammacat.fits.gz')]
+    pathlist = [str(Path('output') / 'gammacat.fits.gz')]
 
     for item in pathlist:
         path = local_repo / item
@@ -146,15 +146,42 @@ class DatasetFermiLat(Dataset):
     files = []
 
     pathlist = [
-        str(Path('3fhl')/'allsky'/'fermi_3fhl_events_selected.fits.gz'),
-        str(Path('3fhl')/'allsky'/'fermi_3fhl_exposure_cube_hpx.fits.gz'),
-        str(Path('3fhl')/'allsky'/'fermi_3fhl_psf_gc.fits.gz'),
-        str(Path('isodiff')/'iso_P8R2_SOURCE_V6_v06.txt')
+        str(Path('3fhl') / 'allsky' / 'fermi_3fhl_events_selected.fits.gz'),
+        str(Path('3fhl') / 'allsky' / 'fermi_3fhl_exposure_cube_hpx.fits.gz'),
+        str(Path('3fhl') / 'allsky' / 'fermi_3fhl_psf_gc.fits.gz'),
+        str(Path('isodiff') / 'iso_P8R2_SOURCE_V6_v06.txt')
     ]
 
     for item in pathlist:
         path = local_repo / item
         jsonpath = str(Path('fermi_3fhl') / Path(item).name)
+        urlpath = path.as_posix().replace(local_repo.as_posix(), "")
+        filesize = os.path.getsize(path)
+        md5 = hashmd5(path)
+        files.append({"path": jsonpath, "url": base_url + urlpath, "filesize": filesize, "hashmd5": md5})
+
+
+class DatasetFermi3FHLGC(Dataset):
+    name = "fermi-3fhl-gc"
+    description = "Prepared Fermi-LAT 3FHL dataset of the Galactic center region"
+    local_repo = Path(os.environ["GAMMAPY_FERMI_LAT_DATA"])
+    base_url = "https://github.com/gammapy/gammapy-fermi-lat-data/raw/master"
+    files = []
+    filenames = [
+        "fermi-3fhl-gc-background.fits.gz",
+        "fermi-3fhl-gc-background-cube.fits.gz",
+        "fermi-3fhl-gc-counts.fits.gz",
+        "fermi-3fhl-gc-counts-cube.fits.gz",
+        "fermi-3fhl-gc-events.fits.gz",
+        "fermi-3fhl-gc-exposure-cube.fits.gz",
+        "fermi-3fhl-gc-psf.fits.gz",
+        "fermi-3fhl-gc-psf-cube.fits.gz",
+        "gll_iem_v06_gc.fits.gz",
+    ]
+
+    for filename in filenames:
+        path = local_repo / "3fhl/galactic-center" / filename
+        jsonpath = str(Path('fermi-3fhl-gc') / filename)
         urlpath = path.as_posix().replace(local_repo.as_posix(), "")
         filesize = os.path.getsize(path)
         md5 = hashmd5(path)

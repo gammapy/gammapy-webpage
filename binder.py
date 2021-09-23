4a220b4a8fe2e3bef341dd43de221eca688e7cb2
Jose Enrique Ruiz <jer@iaa.es>
2021-09-23 20:44:32 +0200

    use mamba to build env in binder docker image

--- binder.py
+++ binder.py
@@ -13,12 +13,12 @@ with open("environment.yml") as stream:
     content = yaml.safe_load(stream)
 
 for chan in content['channels']:
-    print("RUN conda config --add channels {}".format(chan))
-    conda.cli.main('conda', 'config',  '--add', 'channels', chan)
+    print("RUN mamba config --add channels {}".format(chan))
+    conda.cli.main('mamba', 'config',  '--add', 'channels', chan)
 
 for pack in content['dependencies']:
     if isinstance(pack, str):
-        print("RUN conda install -q -y {}".format(pack))
+        print("RUN mamba install -q -y {}".format(pack))
         conda.cli.main('conda', 'install',  '-y', '-q', pack)
     else:
         print("RUN pip install {}".format(pack['pip'][0]))

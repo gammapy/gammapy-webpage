fb10534fcfddb40dd5a3abede6cc767ab71f39be
hstapel <stapel@apc.in2p3.fr>
2024-10-09 08:15:49 +0200

    add Matomo

--- load-scripts.js
+++ load-scripts.js
@@ -1,5 +1,20 @@
 // load-scripts.js
 
+// Load Matomo
+var _paq = window._paq = window._paq || [];
+/* Matomo tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u = "https://apcstatview.in2p3.fr/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
+})();
+
 // Load Google Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag() {
